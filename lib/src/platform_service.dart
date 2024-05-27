27425fd23bea427802d733210aee3bd1d32209f1
Decoder07 <pushpam@100ms.live>
2024-05-27 16:55:02 +0530

    First commit

---
+++ lib/src/platform_service.dart
@@ -0,0 +1,20 @@
+library;
+
+///Package imports
+import 'package:flutter/services.dart';
+
+///Project imports
+import 'package:hms_video_plugin/src/enum/plugin_method.dart';
+
+/// An implementation of [HmsVideoFilterPlatform] that uses method channels.
+class PlatformService {
+  static const _channel = MethodChannel('hms_video_filter');
+
+  ///used to invoke different methods at platform side and returns something but not neccessarily
+  static Future<dynamic> invokeMethod(PluginMethod method,
+      {Map? arguments}) async {
+    var result = await _channel.invokeMethod(
+        PlatformMethodValues.getStringFromPlatformMethod(method), arguments);
+    return result;
+  }
+}
